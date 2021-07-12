e6ebc58d17e2366578ac6a63cf117edb17550573
Ivan <ivanwhitemac@gmail.com>
2021-07-12 22:47:47 +0100

    Finished app

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
-function helloWorld() {
-  alert("Hello World!")
+//Get the value from the string
+function getValue() {
+  document.getElementById("alert").classList.add("invisible")
+
+  let userString = document.getElementById("userString").value
+
+  let reversedString = reverseString(userString)
+
+  displayString(reversedString)
+}
+
+//Reverse the string
+function reverseString(userString) {
+  let revString = []
+
+  for (let i = userString.length - 1; i >= 0; i--) {
+    revString += userString[i]
+  }
+
+  return revString
+}
+
+//Display the message with the reversed string
+function displayString(revString) {
+  document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`
+
+  document.getElementById("alert").classList.remove("invisible")
 }
